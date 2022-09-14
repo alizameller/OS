2a946ca38ddd15123bcdc98343b40f2752188fba
alizameller <alizameller@gmail.com>
2022-09-14 00:22:37 -0400

    added error messages

--- tabstop.c
+++ tabstop.c
@@ -17,6 +17,11 @@ void set_streams(int argc, char** argv, struct MYSTREAM** writeStream, struct MY
             case 'o': 
                 printf("opening for writing: %s\n", optarg); 
                 *writeStream = myfopen(optarg, O_WRONLY, 4096);
+                
+                if (*writeStream == NULL){
+                fprintf(stderr,"Error: could not open %s for writing\n", optarg, strerror(errno));
+                exit(1); 
+            }
                 write = 1; // a write stream is open
                 break;
         }
@@ -26,15 +31,31 @@ void set_streams(int argc, char** argv, struct MYSTREAM** writeStream, struct MY
         *writeStream = myfdopen(STDOUT_FILENO, O_WRONLY, 4096);
     }
 
+    if (*writeStream == NULL){
+        fprintf(stderr,"Error: could not open STDOUT for writing\n", strerror(errno));
+        exit(1); 
+    }
+
     for(; optind < argc; optind++){ 
         printf("opening for reading: %s\n", argv[optind]);
         *readStream = myfopen(argv[optind], O_RDONLY, 4096);
+        
+        if (*readStream == NULL){
+        fprintf(stderr,"Error: could not open %s for reading\n", argv[optind], strerror(errno));
+        exit(1); 
+    }
         read = 1; // a read stream is open
     }
 
     if (!read){ // a read stream is not yet open
         *readStream = myfdopen(STDIN_FILENO, O_RDONLY, 4096);
     }
+
+    if (*readStream == NULL){
+        fprintf(stderr,"Error: could not open STDOUT for reading\n", strerror(errno));
+        exit(1); 
+    }
+
 }
 
 int main(int argc, char** argv){
@@ -43,22 +64,52 @@ int main(int argc, char** argv){
 
     set_streams(argc, argv, &writeStream, &readStream);
 
-    int val;
+    int val = 0;
+    int check = 0;
 
-    while((val = myfgetc(readStream)) != -1) { //check man pages for reaching end of file read
+    while((val = myfgetc(readStream)) != 0) { //check man pages for reaching end of file read
+     
+        if (val == -1) { // this will never be hit help
+            // OUTPUT FILE?!?!
+            fprintf(stderr,"Error: could not get char %c from STDIN\n", check, strerror(errno));
+            exit(1); 
+        }
 
         if (val == '\t'){
             for (int i = 0; i < 4; i++){
-                myfputc(' ', writeStream);
+                check = myfputc(' ', writeStream);
+
+                if (check == -1) {
+                    // OUTPUT FILE?!?!
+                    fprintf(stderr,"Error: could not put char %c in STDOUT\n", check, strerror(errno));
+                    exit(1); 
+                }
             }
+        
             continue;
         }
 
-        myfputc(val, writeStream);
+        check = myfputc(val, writeStream);
+
+        if (check == -1){
+        fprintf(stderr,"Error: could not put char %c in STDOUT\n", check, strerror(errno));
+        exit(1); 
+        }
     }
 
-    myfclose(writeStream);
-    myfclose(readStream);
+    check = myfclose(writeStream);
+
+    if (check == -1){
+        fprintf(stderr,"Error: could not close STDOUT\n", strerror(errno));
+        exit(1); 
+    }
+
+    check = myfclose(readStream);
+
+    if (check == -1){
+        fprintf(stderr,"Error: could not close STDIN\n", strerror(errno));
+        exit(1); 
+    }
 
     return 0;
 }
