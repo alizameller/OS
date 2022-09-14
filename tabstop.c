fe4438df0d552e818f6069844d94fbd1c2e433e6
alizameller <alizameller@gmail.com>
2022-09-14 01:04:26 -0400

    Problems 3 and 4 complete

--- tabstop.c
+++ tabstop.c
@@ -9,45 +9,44 @@ void set_streams(int argc, char** argv, struct MYSTREAM** writeStream, struct MY
     while((opt = getopt(argc, argv, "o:")) != -1) {
         switch (opt) { 
             case 'o': 
-                printf("opening for writing: %s\n", optarg); 
+                //printf("opening for writing: %s\n", optarg); 
                 *writeStream = myfopen(optarg, O_WRONLY, 4096);
                 
                 if (*writeStream == NULL){
-                fprintf(stderr,"Error: could not open %s for writing\n", optarg, strerror(errno));
-                exit(1); 
-            }
+                    fprintf(stderr,"Error: could not open %s for writing\n", optarg, strerror(errno));
+                    exit(1); 
+                }
                 write = 1; // a write stream is open
-                break;
         }
     }
 
     if (!write){ // a write stream is not yet open 
         *writeStream = myfdopen(STDOUT_FILENO, O_WRONLY, 4096);
-    }
 
-    if (*writeStream == NULL){
-        fprintf(stderr,"Error: could not open STDOUT for writing\n", strerror(errno));
-        exit(1); 
+        if (*writeStream == NULL){
+            fprintf(stderr,"Error: could not open STDOUT for writing\n", strerror(errno));
+            exit(1); 
+        }
     }
 
     for(; optind < argc; optind++){ 
-        printf("opening for reading: %s\n", argv[optind]);
+        //printf("opening for reading: %s\n", argv[optind]);
         *readStream = myfopen(argv[optind], O_RDONLY, 4096);
         
         if (*readStream == NULL){
-        fprintf(stderr,"Error: could not open %s for reading\n", argv[optind], strerror(errno));
-        exit(1); 
-    }
+            fprintf(stderr,"Error: could not open %s for reading\n", argv[optind], strerror(errno));
+            exit(1); 
+        }
         read = 1; // a read stream is open
     }
 
     if (!read){ // a read stream is not yet open
         *readStream = myfdopen(STDIN_FILENO, O_RDONLY, 4096);
-    }
 
-    if (*readStream == NULL){
-        fprintf(stderr,"Error: could not open STDOUT for reading\n", strerror(errno));
-        exit(1); 
+        if (*readStream == NULL){
+            fprintf(stderr,"Error: could not open STDOUT for reading\n", strerror(errno));
+            exit(1); 
+        }
     }
 }
 
@@ -60,8 +59,7 @@ int main(int argc, char** argv){
     int val = 0;
     int check = 0;
 
-    while((val = myfgetc(readStream)) != 0) { 
-     
+    while(val = myfgetc(readStream)) { //!= 0
         if (val == -1) {
             if (readStream->fd == STDIN_FILENO){
                 fprintf(stderr,"Error: could not get char from STDIN\n", strerror(errno));
@@ -77,10 +75,10 @@ int main(int argc, char** argv){
 
                 if (check == -1) {
                     if (writeStream->fd == STDOUT_FILENO){
-                        fprintf(stderr,"Error: could not write char to STDOUT\n", strerror(errno));
+                        fprintf(stderr,"Error: could not put char to STDOUT\n", strerror(errno));
                         exit(1); 
                     } 
-                    fprintf(stderr,"Error: could not write char to file\n", strerror(errno));
+                    fprintf(stderr,"Error: could not put char to file\n", strerror(errno));
                     exit(1); 
                 }
             }
@@ -92,10 +90,10 @@ int main(int argc, char** argv){
 
         if (check == -1){
             if (writeStream->fd == STDOUT_FILENO){
-                fprintf(stderr,"Error: could not write char to STDOUT\n", strerror(errno));
+                fprintf(stderr,"Error: could not put char to STDOUT\n", strerror(errno));
                 exit(1); 
             } 
-            fprintf(stderr,"Error: could not write char to file\n", strerror(errno));
+            fprintf(stderr,"Error: could not put char to file\n", strerror(errno));
             exit(1); 
         }
     }
