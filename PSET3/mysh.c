37de163d07f2aef5fc8cc74f2de737e8c35fa0e1
alizameller <alizameller@gmail.com>
2022-10-23 00:50:59 -0400

    updates to cd, pwd, exit

--- PSET3/mysh.c
+++ PSET3/mysh.c
@@ -17,7 +17,7 @@ void redirIO(char **args) {
 }
 
 void shelliza_cd(char **args) {
-    char *path; 
+    char *path;
     if (args[1] == NULL) {
         char *envvar = "HOME";
         if (!(path = getenv(envvar))){
@@ -27,15 +27,30 @@ void shelliza_cd(char **args) {
     } else {
         path = args[1];
     }
-    // relative or full path?
+    if (chdir(path) == -1) {
+        fprintf(stderr, "Could not change directory to path %s: %s\n", path, strerror(errno));
+        exit(1);
+    }
 }
 
 void shelliza_pwd(char **args) {
-
+    char *string;
+    if (!(string = getcwd(NULL, 0))) {
+        fprintf(stderr, "Could not get current working directory: %s\n", strerror(errno));
+        exit(1);
+    }
+    printf("%s\n", string);
+    free(string);
 }
 
 void shelliza_exit(char **args) {
-
+    int status;
+    if (args[1] == NULL || args[0] == NULL) {
+        status = 0; //change this to exit status of last command
+    } else {
+        status = atoi(args[1]);
+    }
+    exit(status);
 }
 
 // Using an array of structs to map the command name to its respective function 
@@ -61,7 +76,7 @@ char** tokenization(char *line) {
     }
 
     for (i = 0, str1 = line; ; i++, str1 = NULL) {
-        string[i] = strtok(str1, " \t");
+        string[i] = strtok(str1, " \t\n");
         if (!string[i]) {
             break;
         }
@@ -71,9 +86,7 @@ char** tokenization(char *line) {
 }
 
 void shelliza_exec(char **args) {
-/*    int i;
-    int numFuncs = 3; 
-    int wstatus; 
+/*  int wstatus; 
     //split args into commands and IO redirection
     pid_t childPid = fork();
 
@@ -81,12 +94,6 @@ void shelliza_exec(char **args) {
         // do IO redirection ?
         redirIO(args); 
         // execute commands
-        for (i = 0; i < numFuncs; i++)
-            // execute respective commands inside child process
-            if (!strcmp(args[0], functions[i].name)) { //if the command matches a command in the functions map (struct)
-                functions[i].func(args); //call the respective function
-            }
-        exit(1); 
     } else if (childPid > 0) { // parent process
         do {
             if (wait(&wstatus) == -1) {
@@ -105,6 +112,16 @@ void shelliza_exec(char **args) {
     } */
 }
 
+void shelliza_builtin(char **args) {
+    int i; 
+    int numFuncs = 3; 
+    for (i = 0; i < numFuncs; i++) {
+        if (!strcmp(args[0], functions[i].name)) { //if the command matches a command in the functions map (struct)
+            functions[i].func(args); //call the respective function
+        }
+    }
+}
+
 void driver(FILE *stream) {  
     int i;
     char *buffer; 
@@ -117,7 +134,9 @@ void driver(FILE *stream) {
     }
 
     while (1) {
-        printf("[shelliza:~]$ ");
+        if (stream == stdin) {
+            printf("[shelliza:~]$ ");
+        }
         if (getline(&buffer, &bufsize, stream) == -1) {
             break;
         }
@@ -127,13 +146,14 @@ void driver(FILE *stream) {
         if (*tokens[0] == '#') {
             continue; 
         }
-
+        /*
         for (i = 0; tokens[i]; i++) {
             printf("%s\n", tokens[i]);
         }
-
+        */
         if (tokens[0] != NULL) {
-            shelliza_exec(tokens);
+            shelliza_builtin(tokens);
+            //shelliza_exec(tokens);
         }
 
         free(tokens); 
@@ -155,15 +175,15 @@ int main(int argc, char **argv) {
     char *pathname;
     int fd;
 
-    if (pathname = argv[1] != NULL) {
-        stream = fopen(pathname, 'r');
-    }
-        //set file names
-    // else if not shell script
+    // if shell script
+    if ((pathname = argv[1]) != NULL) {
+        stream = fopen(pathname, "r");
+    } else { // else if not shell script
         //inFileName = "STDIN"; 
         //outFileName = "STDOUT"; 
         stream = stdin; 
-    
+    }
+
     driver(stream); 
 
     return 0;
